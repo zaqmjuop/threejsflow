4f007efe041b54fdaee3635901f62343458957ea
zhubochao <1060007045@qq.com>
2023-01-07 03:45:58 +0800

    chore: add vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 let buildType = 'lib'
 
 export default defineConfig({
+  plugins: [vue()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }]
   },
@@ -16,7 +18,6 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['@zaqmjuop/threejsflow']
   },
-  plugins: [],
   build: {
     outDir: buildType === 'lib' ? 'dist' : 'docs',
     lib:
