d819b9387fc9b4593ee1ead7647d16a09c07f80c
zhubochao <1060007045@qq.com>
2023-01-07 03:05:48 +0800

    chore: add config

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+let buildType = 'lib'
+
+export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }]
+  },
+  server: {
+    port: 3000,
+    watch: {
+      ignored: ['!**/node_modules/@zaqmjuop/libgif/dist/libgif.mjs']
+    }
+  },
+  optimizeDeps: {
+    exclude: ['@zaqmjuop/libgif']
+  },
+  plugins: [],
+  build: {
+    outDir: buildType === 'lib' ? 'dist' : 'docs',
+    lib:
+      buildType === 'lib'
+        ? {
+            entry: path.resolve(__dirname, 'lib/libgif.ts'),
+            name: 'libgif',
+            formats: ['es'],
+            fileName: 'libgif'
+          }
+        : void 0
+  },
+  base: 'libgif', // git-page 基础路径
+  preview: {
+    port: 8080
+  }
+})
