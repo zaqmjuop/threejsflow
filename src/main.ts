36e2479b76c849b5ac85317f31fec9ae1648e08b
zhubochao <1060007045@qq.com>
2023-01-11 01:14:53 +0800

    feat: troisjs

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-import './style.css'
-import WebGL from 'three/examples/jsm/capabilities/WebGL.js'
-import App from './app'
+// import './style.css'
+// import WebGL from 'three/examples/jsm/capabilities/WebGL.js'
+// import App from './app'
 
-const mounted = () => {
-  if (!WebGL.isWebGLAvailable()) {
-    const warning = WebGL.getWebGLErrorMessage()
-    document.body.appendChild(warning)
-  } else {
-    const app = App() 
-    document.body.appendChild(app.renderer.domElement)
-    app.setup()
-    app.mounted()
-  }
-}
+// const mounted = () => {
+//   if (!WebGL.isWebGLAvailable()) {
+//     const warning = WebGL.getWebGLErrorMessage()
+//     document.body.appendChild(warning)
+//   } else {
+//     const app = App() 
+//     document.body.appendChild(app.renderer.domElement)
+//     app.setup()
+//     app.mounted()
+//   }
+// }
 
-mounted()
+// mounted()
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
