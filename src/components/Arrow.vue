7d7c3ecb457ac10995934751dddc3e8c8058d371
zhubochao <1060007045@qq.com>
2023-01-22 16:42:41 +0800

    feat: 3方向箭头

--- src/components/Arrow.vue
+++ src/components/Arrow.vue
@@ -2,14 +2,15 @@
   <Group
     :position="{
       x: position.x,
-      y: position.y + 20,
+      y: position.y,
       z: position.z
     }"
+    :rotation="rotation"
   >
     <Cylinder
       :position="{
         x: 0,
-        y: 0,
+        y: 5,
         z: 0
       }"
       :height="10"
@@ -17,7 +18,7 @@
       :radiusBottom="1"
     >
       <PhongMaterial
-        color="#156289"
+        :color="color"
         :props="{
           emissive: 0x072534,
           opacity: 0.7,
@@ -29,14 +30,14 @@
     <Cone
       :position="{
         x: 0,
-        y: 0,
+        y: 12.5,
         z: 0
       }"
       :height="5"
       :radius="1.5"
     >
       <PhongMaterial
-        color="#156289"
+        :color="color"
         :props="{
           emissive: 0x072534,
           opacity: 0.7,
@@ -47,13 +48,17 @@
   ></Group>
 </template>
 <script lang="ts" setup>
-import { Cylinder, Cone, PhongMaterial } from 'troisjs'
+import { Cylinder, Cone, PhongMaterial, Group } from 'troisjs'
 import { PropType } from 'vue'
 
 defineProps({
   color: {
     type: String,
     default: ''
+  },
+  rotation: {
+    type: Object as PropType<{ x: number; y: number; z: number }>,
+    default: () => ({ x: 0, y: 0, z: 0 })
   },
   position: {
     type: Object as PropType<{ x: number; y: number; z: number }>,
--- src/components/DragControll.vue
+++ src/components/DragControll.vue
@@ -1,11 +1,24 @@
 <template>
-  <Arrow :position="position"></Arrow>
-  <Arrow :position="position"></Arrow>
-  <Arrow :position="position"></Arrow>
+  <Arrow
+    :color="'#ff0000'"
+    :position="position"
+    :rotation="{ x: 0, y: 0, z: getDegVal(90) }"
+  ></Arrow>
+  <Arrow
+    :color="'#00ff00'"
+    :position="position"
+    :rotation="{ x: getDegVal(90), y: 0, z: 0 }"
+  ></Arrow>
+  <Arrow
+    :color="'#0000ff'"
+    :position="position"
+    :rotation="{ x: 0, y: 0, z: 0 }"
+  ></Arrow>
 </template>
 <script setup lang="ts">
 import { PropType } from 'vue'
 import Arrow from '@/components/Arrow.vue'
+import { getDegVal } from '@/common/getDegVal'
 
 defineProps({
   color: {
